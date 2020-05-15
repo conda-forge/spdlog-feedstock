7c6fea7cf8d080e13cdd8e56e68459d556be137d
Rong Ou <rong.ou@gmail.com>
2020-05-15 10:34:34 -0700

    fix build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
-make
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX -D SPDLOG_BUILD_TESTS=ON ..
+make -j${CPU_COUNT}
 make test
 make install
