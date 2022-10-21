1870774868b18368930bc083decfa2982e2240c1
Francesco Biscani <bluescarni@gmail.com>
2022-10-21 23:13:47 +0200

    De-vendor fmt.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D SPDLOG_BUILD_SHARED=ON ..
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D SPDLOG_BUILD_SHARED=ON -D SPDLOG_FMT_EXTERNAL=ON ..
 if errorlevel 1 exit 1
 
 nmake
